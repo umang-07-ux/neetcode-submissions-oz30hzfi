db28d0a5fad5b2da61ec2a9ddb4241d8fa288dbe
Umang Srivastava <umang2897@gmail.com>
2026-08-06 02:06:28 +0530

    Add: contains-duplicate-ii - submission-2

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        int n=nums.length;
+        HashMap<Integer,Integer>  freq=new HashMap<>();
+        HashMap<Integer,Integer> storeindex=new HashMap<>();
+        int index=Integer.MIN_VALUE;
+        boolean flag=false;
+        for(int i=0;i<n;i++) {
+            int x=nums[i];
+            freq.put(x,freq.getOrDefault(x,0)+1);
+            if(freq.get(x)>1) {
+                int j=storeindex.get(nums[i]);
+                index=Math.abs(j-i);
+                if(index<=k) {
+                    flag=true;
+                }
+            }
+            storeindex.put(nums[i],i);
+        }
+        return flag;
+    }
+}
